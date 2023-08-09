e840d2c7b57a8e351310f2a6b5623941e196abd1
nagaraju138m <52023391+nagaraju138m@users.noreply.github.com>
2023-08-09 11:00:28 +0530

    randon changes

--- Sample1/Program.cs
+++ Sample1/Program.cs
@@ -14,8 +14,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
 builder.Services.AddScoped<IStudnetRepository, StudnetRepository>();
-builder.Services.AddScoped(typeof(IGenericRepository<>),typeof(GenericRepository<>));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
