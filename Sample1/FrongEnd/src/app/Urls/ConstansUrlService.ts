54e3f61c6dc6be39de8ae86fc3dad07918a1e00c
nagaraju138m <52023391+nagaraju138m@users.noreply.github.com>
2023-09-07 18:07:37 +0530

    add book stu studnet frontend

--- Sample1/FrongEnd/src/app/Urls/ConstansUrlService.ts
+++ Sample1/FrongEnd/src/app/Urls/ConstansUrlService.ts
@@ -10,10 +10,9 @@ export namespace ConstansUrlService{
     export const deleteStudent = "/Student"
 
     // books
-    export const getBooksById = /Book/getBooksById
+    export const getBooksById = "/Book/getBooksById"
     export const getbooks = "/Book/getbooks"
     export const PostBooks = "/Book/PostBooks"
-
 
 
 }
